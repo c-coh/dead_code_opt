9d0aa089b87f66c2cda5f048e4f5fc2b9a1f31bd
cld99 <115433496+cld99@users.noreply.github.com>
2024-05-04 11:05:20 -0400

    Added test case for dead assignment corner cases

---
+++ new-tests/test5.c
@@ -0,0 +1,17 @@
+int printf(string fmt, ...);
+
+float add(int a, float b)
+{
+    printf("%f\n", a + b);
+    return a + b;
+}
+
+int main()
+{
+    float a;
+    int b;
+    int c;
+    a = add(1, 2.3);
+    b = c = 4;
+    return c;
+}
